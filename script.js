3cc7ac5f7b71d29500c92580ecbad4e2947dc8f2
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-06-06 15:18:21 +0200

    updated script

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const button = document.querySelector(".claim");
 const input = document.querySelectorAll("input");
 const label = document.querySelectorAll("label");
+let something = ""
+function label(){
+    if (input == something){
 
-button.forEach(
-  
-)
+    }
+}
