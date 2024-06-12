97ab863ef834f096f095e22de5f5c797144eb597
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-06-12 15:14:44 +0200

    updated script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 const button = document.querySelector(".claim");
-const input = document.querySelectorAll("input");
-const label = document.querySelectorAll("label");
-let something = ""
-function label(){
-    if (input == something){
+const inputName = document.querySelector(".first-name");
+const labelName = document.querySelector(".name");
+const inputSurname = document.querySelector(".lastname");
+const labelSurname = document.querySelector(".last");
+const inputEmail = document.querySelector(".email");
+const labelEmail = document.querySelector(".labeEmail");
+const inputPassword = document.querySelector(".password");
+const labelPassword = document.querySelector(".labelPassword");
+let nothing = "";
 
-    }
-}
+
+function validate (){
+    if (inputName === nothing){
+        labelName.style.display = "inline"
+        };
+        if (inputSurname === nothing){
+            labelSurname.style.display = "block"
+            };
+            if (inputEmail === nothing){
+                labelEmail.style.display = "block"
+                };
+                if (inputPassword === nothing){
+                    labelPassword.style.display = "block"
+                    };
+                    }
+button.addEventListener("click", validate() )
+ 
