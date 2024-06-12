4d1a10dbb5c1fec4e75c397e42de9a9996347689
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-06-12 18:51:24 +0200

    updated script.js

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-const button = document.querySelector(".claim");
-const inputName = document.querySelector(".first-name");
-const labelName = document.querySelector(".name");
-const inputSurname = document.querySelector(".lastname");
-const labelSurname = document.querySelector(".last");
-const inputEmail = document.querySelector(".email");
-const labelEmail = document.querySelector(".labeEmail");
-const inputPassword = document.querySelector(".password");
-const labelPassword = document.querySelector(".labelPassword");
-let nothing = "";
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector(".claim");
+    const inputName = document.querySelector(".first-name");
+    const labelName = document.querySelector(".name");
+    const inputSurname = document.querySelector(".last");
+    const labelSurname = document.querySelector(".lastName");
+    const inputEmail = document.querySelector(".email");
+    const labelEmail = document.querySelector(".labelEmail");
+    const inputPassword = document.querySelector(".password");
+    const labelPassword = document.querySelector(".labelPassword");
 
 
-function validate (){
-    if (inputName === nothing){
-        labelName.style.display = "inline"
-        };
-        if (inputSurname === nothing){
-            labelSurname.style.display = "block"
-            };
-            if (inputEmail === nothing){
-                labelEmail.style.display = "block"
-                };
-                if (inputPassword === nothing){
-                    labelPassword.style.display = "block"
-                    };
-                    }
-button.addEventListener("click", validate() )
- 
+
+    function validate() {
+        let nothing = "";
+
+        if (inputName.value === nothing) {
+            labelName.style.visibility = "visible";
+            
+        } 
+        if (inputSurname.value === nothing) {
+            labelSurname.style.visibility = "visible";
+        } 
+        if (inputEmail.value === nothing || !validateEmail(inputEmail.value)) {
+            labelEmail.style.visibility = "visible";
+        } 
+        if (inputPassword.value === nothing) {
+            labelPassword.style.visibility = "visible";
+        } 
+    }
+
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    button.addEventListener("click", validate);
+});
